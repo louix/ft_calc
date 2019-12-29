ad23c22de68332e2bbdd420852cfbe2b726e34a6
louix <28579246+louix@users.noreply.github.com>
2019-12-29 22:21:11 +0000

    added money and time to run

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process;
 fn main() {
     let money = 100;
     let time = 10; // Minutes
-    if let Err(e) = ft_calculator::run() {
+    if let Err(e) = ft_calculator::run(money, time) {
         eprintln!("Error! {}", e);
         process::exit(1);
     }
